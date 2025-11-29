0ee6778b835bde3b19aa9037fd4fa739d45c4175
Victor Gonzalez <vemiliogp@gmail.com>
2025-11-29 20:14:42 +0100

    feat: add API configuration example and update .gitignore for environment files

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
